04c5e76df48433f444fdde6e5e68bad61b4fffb9
Meghan Domeck <mdomeck@aol.com>
2020-07-21 15:25:50 -0400

    code along

--- data/books.sql
+++ data/books.sql
@@ -5,3 +5,14 @@
 -- image_url
 -- description
 -- bookshelf
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  author VARCHAR(255),
+  title VARCHAR(255),
+  isbn VARCHAR(255),
+  image_url
+  description TEXT,
+  bookshelf
+);
