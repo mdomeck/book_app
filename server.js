123bab069ad68f4550eedf8519da5adeaaf1f203
Hexx King <hexxking13@gmail.com>
2020-07-21 18:07:45 -0700

    end of day. workingon step1

--- server.js
+++ server.js
@@ -26,66 +26,60 @@ app.use(express.static('./public')); //where out static front end is going to li
 app.use(express.urlencoded({extended: true})); // decodes out response.body- body parser
 
 // routes
-
-
-
-app.get('/', renderHomePage);
-
-// app.get('/books/:task_id', getOneTask);
-// app.get('/app', showForm);
-// app.post('/add', addTask);
-
+// app.get('/', renderHomePage);
+app.get('/books/:books_id', getOneBook);
+app.get('/app', showResults);
+app.post('/add', addBook);
 app.get('/searches/new', renderSearchPage);
-
 app.post('/searches', collectSearchResults);
 
 
-// function getAllFromDatabase(request, response) {
-//   let sql = 'SELECT * FROM books;';
-//   client.query(sql)
-//   .then(results => {
-//     let books = results.rows;
-//     response.render('index.ejs', {results: books});
-//   })
-// }
+function getAllFromDatabase(request, response) {
+  let sql = 'SELECT * FROM books;';
+  client.query(sql)
+  .then(results => {
+    let books = results.rows;
+    response.render('index.ejs', {results: books});
+  })
+}
 
-// function getOneTask(request, response){
-//   console.log('this is my request.params:', request.params);
+function getOneBook(request, response){
+  console.log('this is my request.params:', request.params);
 
-//   let id = request.params.task_id;
+  let id = request.params.books_id;
 
-//   let sql = 'SELECT * FROM books WHERE id=$1;';
-//   let safeValues = [id];
+  let sql = 'SELECT * FROM books WHERE id=$1;';
+  let safeValues = [id];
 
-//   client.query(sql, safeValues)
-//   .then(results => {
-//     console.log('this should be the task that I selected', results.rows);
-//     let selectedTask = results.rows[0];
-//     response.render('pages/detail-view', {task:selectedTask});
-//   })
-// }
+  client.query(sql, safeValues)
+  .then(results => {
+    console.log('this should be the book that I selected', results.rows);
+    let selectedBooks = results.rows[0];
+    response.render('./pages/index', {books:selectedBooks});
+  })
+  // return(selectedBooks.id);
+}
 
-// function showForm(request, response){
-//   response.render('pages/');
-// }
+function showResults(request, response){
+  response.render('pages/books');
+}
 
-// function addTask(request, response){
-//   let formData = request.body;
-//   console.log('This is our form data', formData);
-//   let {title, desription} = request.body
+function addBook(request, response){
+  let formData = request.body;
+  console.log('This is our form data', formData);
+  let {title, desription} = request.body
 
-//   let sql = 'INSERT INTO books (title, description) VALUES ($1, $2)RETURNING ID;';
+  let sql = 'INSERT INTO books (title, author, description, isbn, image_url, bookshelf) VALUES ($1, $2, $3, $4, $5, $6)RETURNING ID;';
 
-//   let safeValues = [title, description];
+  let safeValues = [title, author, description, isbn,image_url, bookshelf];
 
-//   client.query(sql, safeValues)
-//   .then(results => {
-//     let id = results.rows[0];
-//     console.log('this should be an id', id);
-
-//     response.redirect(`/books/${id}`);
-//   })
-// }
+  client.query(sql, safeValues)
+  .then(results => {
+    let id = results.rows[0];
+    console.log('this should be an id', id);
+    response.redirect(`/books/${id}`);
+  })
+}
 
 
 // functions
@@ -137,7 +131,9 @@ superagent.get(url)
     this.image = obj.imageLinks ? obj.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
     this.authors = obj.authors ? obj.authors[0] : 'no author available';
     this.description = obj.description ? obj.description : 'no description available';
-
+    this.isbn = obj.industryIdentifiers[0].identifier;
+    // this.bookshelf = obj.industryIdentifiers.categories[0];
+    // console.log(obj.industryIdentifiers.categories[0]);
   }
 
 app.use('*', (request, response) => {
